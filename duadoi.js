e00947b16b7efc0f994ce79721796a0c5d6b6944
kenleo0508 <85601582+kenleo0508@users.noreply.github.com>
2021-06-27 14:53:32 +0700

    Update duadoi.js

--- duadoi.js
+++ duadoi.js
@@ -15,8 +15,8 @@ module.exports.config = {
 module.exports.run = async ({api, event}) => {
     var cron = require('node-cron');
  
- cron.schedule('0 56 10 * * SAT-SUN',event.threadID,  () => {
-   api.sendMessage('!ping đua đội',event.threadID,event.messageID);
+ cron.schedule('0 15 * * SAT-SUN', () => {
+   api.sendMessage('!ping',event.threadID,event.messageID);
  }, {
    scheduled: true,
    timezone: "Asia/Ho_Chi_Minh"
