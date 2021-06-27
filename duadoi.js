f0bb25b8622dedabc6748cdf3d49ba01045b2f64
kenleo0508 <85601582+kenleo0508@users.noreply.github.com>
2021-06-27 10:46:26 +0700

    Add files via upload

--- duadoi.js
+++ duadoi.js
@@ -7,17 +7,19 @@ module.exports.config = {
     commandCategory: "system",
     cooldowns: 5;
     dependencies: {
-        "node-schedule": ""
+        "node-cron": ""
     }
 };
 
 
 module.exports.run = async ({api, event}) => {
-    var schedule = require('node-schedule');
-    var cronExpress = '0 50 7 * * SAT-SUN';
-    var j =  schedule.scheduleJob(cronExpress, function(fireDate) {
-        return api.sendMessage("!ping đua đội",event.threadID,event.messageID);
-        return api.sendMessage(fireDate,event.threadID,event.messageID)
-    }); 
+    var cron = require('node-cron');
+ 
+ cron.schedule('0 50 7 * * SAT-SUN', () => {
+   api.sendMessage('!ping đua đội',event.threadID,event.messageID);
+ }, {
+   scheduled: true,
+   timezone: "Asia/Ho_Chi_Minh"
+ });
 }
 
