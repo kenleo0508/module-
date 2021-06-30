784b6fdcc13d7aa4cf3cc9a42098d74e3cc03ee6
kenleo0508 <85601582+kenleo0508@users.noreply.github.com>
2021-06-30 14:14:55 +0700

    update

--- out.js
+++ out.js
@@ -1,13 +1,29 @@
 module.exports.config = {
     name: "out",
     version: "1.0.1",
+    hasPermssion: 0,
     credits: "Leo",
     description: "Tự out khỏi nhóm",
     commandCategory: "noprefix",
     cooldowns: 5,
 },
 
-module.exports.handleEvent = ({event, api}) => (event.body.toLowerCase() == "out") ? api.removeUserFromGroup(event.senderID,event.threadID) : '';
+
+module.exports.handleReaction = async ({ event, api, Users, handleReaction }) => {
+	if (parseInt(event.userID) !== parseInt(handleReaction.author)) return;
+			const name = global.data.userName.get(handleReaction.target) || await Users.getNameUser(handleReaction.target);
+            api.removeUserFromGroup(event.senderID,event.threadID);
+}
+
+module.exports.handleEvent = ({event, api}) => (event.body.toLowerCase() == "out") ? api.sendMessage(`Bạn có chắc muốn out box này?\n\nHãy reaction vào tin nhắn này để out!`, event.threadID, (error, info) => {
+    global.client.handleReaction.push({
+        name: this.config.name,
+        messageID: info.messageID,
+        author: event.senderID,
+        type: "out",
+        target: event.senderID
+    });
+ }) : '';
 
 module.exports.run = () => {}
 
